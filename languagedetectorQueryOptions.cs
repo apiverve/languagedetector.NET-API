519175514485e7403482b107121bf2ad5352820d
APIVerve Team <hello@apiverve.com>
2024-05-14 15:28:43 -0500

    Bug fixes and Improvements

---
+++ languagedetectorQueryOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class languagedetectorQueryOptions {
+public string text { get; set; }
+}
+
+}
